7edd79085c78e92fbf7c1e751d36a221a2deb513
Agustin Carrasco <asermax@gmail.com>
2020-01-31 23:35:53 -0300

    initial commit

---
+++ src/shiro/urls.py
@@ -0,0 +1,48 @@
+"""shiro URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+from user import urls as user_urls
+from checkout import urls as checkout_urls
+
+api_patterns = [
+    path('api/', include(user_urls)),
+    path('api/', include(checkout_urls)),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/openapi/', get_schema_view(
+        title="Shiro",
+        description="WebConf Checkout API",
+        version="1.0.0",
+        patterns=api_patterns,
+    ), name='openapi-schema'),
+    path('api/swagger/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'},
+    ), name='swagger-ui'),
+] + api_patterns
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
