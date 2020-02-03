8efb77425af4071dacb118b01d7539a1e8a098da
Agustin Carrasco <asermax@gmail.com>
2020-02-03 00:15:58 -0300

    feat(checkout): add string representation to all models

--- src/checkout/models.py
+++ src/checkout/models.py
@@ -16,6 +16,9 @@ class Item(models.Model):
     stock = models.PositiveIntegerField(default=0)
     cancellable = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 
 class ItemOption(models.Model):
     TYPES = choices.Choices(('TEXT', 'Texto'), ('EMAIL', 'Email'), ('COLOR', 'Color'))
@@ -25,10 +28,13 @@ class ItemOption(models.Model):
     type = models.CharField(max_length=10, choices=TYPES)
     possible_values = postgres_fields.JSONField(
         null=True,
+        blank=True,
         validators=[custom_validators.validate_list],
     )
 
-        blank=True,
+    def __str__(self):
+        return f'{self.item} option: {self.name}'
+
 
 class DiscountCode(models.Model):
     TYPES = choices.Choices(('ITEM', 'Item'), ('ORDER', 'Orden'))
@@ -43,6 +49,9 @@ class DiscountCode(models.Model):
         validators=[validators.MaxValueValidator(100)],
     )
     fixed_value = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.code} ({self.description})'
 
 
 class DiscountCodeRestriction(models.Model):
@@ -61,6 +70,9 @@ class DiscountCodeRestriction(models.Model):
     type = models.CharField(max_length=10, choices=TYPES)
     value = postgres_fields.JSONField(validators=[custom_validators.validate_single_value])
 
+    def __str__(self):
+        return f'{self.type} restriction for {self.discount_code}'
+
 
 class Customer(models.Model):
     user = models.OneToOneField(
@@ -69,6 +81,9 @@ class Customer(models.Model):
     )
     identity_document = models.CharField(max_length=50)
     company = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Order(models.Model):
@@ -83,6 +98,9 @@ class Order(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     notes = models.TextField()
 
+    def __str__(self):
+        return f'Orden {self.id} ({self.customer})'
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='items')
@@ -94,17 +112,26 @@ class OrderItem(models.Model):
     price = money_fields.MoneyField(max_digits=7, decimal_places=2, default_currency='ARS')
     fulfilled = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.amount} {self.item} ({self.price})'
+
 
 class OrderItemOption(models.Model):
     order_item = models.ForeignKey('OrderItem', on_delete=models.CASCADE, related_name='options')
     item_option = models.ForeignKey('ItemOption', on_delete=models.CASCADE, related_name='+')
     value = postgres_fields.JSONField(validators=[custom_validators.validate_single_value])
 
+    def __str__(self):
+        return f'{self.item_option}: {self.value}'
+
 
 class Invoice(models.Model):
     order = models.OneToOneField('Order', on_delete=models.CASCADE)
     number = models.PositiveIntegerField()
     file = models.FileField(upload_to='invoices/')
+
+    def __str__(self):
+        return f'Factura {self.number} de {self.order}'
 
 
 class Payment(models.Model):
@@ -122,6 +149,9 @@ class Payment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Pago {self.external_id} de {self.order}'
+
 
 class Cancellation(models.Model):
     order = models.OneToOneField('Order', on_delete=models.CASCADE)
@@ -134,16 +164,25 @@ class Cancellation(models.Model):
         through='CancellationItem',
     )
 
+    def __str__(self):
+        return f'Cancelación de {self.order}'
+
 
 class CancellationItem(models.Model):
     cancellation = models.ForeignKey('Cancellation', on_delete=models.CASCADE, related_name='items')
     order_item = models.OneToOneField('OrderItem', on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return f'Cancelación de {self.order_item}'
 
 
 class CreditNote(models.Model):
     cancellation = models.OneToOneField('Cancellation', on_delete=models.CASCADE)
     number = models.PositiveIntegerField()
     file = models.FileField(upload_to='credit_notes/')
+
+    def __str__(self):
+        return f'Nota de crédito de {self.cancellation.order}'
 
 
 class Refund(models.Model):
@@ -157,3 +196,6 @@ class Refund(models.Model):
     status = util_fields.StatusField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Refund de {self.cancellation.order}'
