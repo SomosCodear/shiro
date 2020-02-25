368ee2138b3a0eb37ce903f89c77cd184801803d
Agustin Carrasco <asermax@gmail.com>
2020-02-25 20:45:06 -0300

    chore(dev settings): use console backend for emails on development

--- src/shiro/settings/development.py
+++ src/shiro/settings/development.py
@@ -7,3 +7,5 @@ CACHES['afip'] = {  # noqa
     'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
     'LOCATION': 'afip_cache_table',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
