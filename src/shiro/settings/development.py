57a9c2ad2163f5348f232a1ba3d45b11498b9fb2
Agustin Carrasco <asermax@gmail.com>
2020-03-02 02:00:02 -0300

    chore(settings): configure corsheaders for dev

--- src/shiro/settings/development.py
+++ src/shiro/settings/development.py
@@ -1,9 +1,20 @@
 """
 Django develop settings for shiro project.
 """
-from .base import *  # noqa
+from .base import *  # noqa: F403
 
-CACHES['afip'] = {  # noqa
+# CORS
+INSTALLED_APPS += [  # noqa: F405
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+] + MIDDLEWARE  # noqa: F405
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CACHES['afip'] = {  # noqa: F405
     'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
     'LOCATION': 'afip_cache_table',
 }
