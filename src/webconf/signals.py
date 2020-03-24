a1f2819224bda0f4941577ba5a1f14a3e040b91b
Agustin Carrasco <asermax@gmail.com>
2020-03-24 00:48:31 -0300

    refactor(pass): use type instead of name to retrieve the email

--- src/webconf/signals.py
+++ src/webconf/signals.py
@@ -5,7 +5,7 @@ from checkout import models, signals as checkout_signals
 
 def send_pass_emails(sender, order=None, **kwargs):
     for order_item in order.order_items.filter(item__type=models.Item.TYPES.PASS):
-        email = order_item.options.get(item_option__name='email').value
+        email = order_item.options.get(item_option__type=models.ItemOption.TYPES.EMAIL).value
         templated_email.send_templated_mail(
             template_name='pass_paid',
             from_email=settings.DEFAULT_EMAIL,
